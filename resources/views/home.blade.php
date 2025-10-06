78f00057c0dc48d830f40f2880d682ef3eff3018
MuhamadYahya <121210925+yahyas123@users.noreply.github.com>
2025-10-06 20:45:34 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores quasi mollitia rerum fuga, esse iure cupiditate molestiae rem quis dolores, sapiente temporibus. Harum suscipit, doloribus sapiente beatae voluptates fugit reprehenderit!</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>I call My Home</h1>
+    <p>Ini adalah konten untuk halaman home. Lorem ipsum dolor sit amet...</p>
+@endsection
