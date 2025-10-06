2d9b065a2354cc315193f8bd6993316d1c57e955
MuhamadYahya <121210925+yahyas123@users.noreply.github.com>
2025-10-06 20:46:43 +0700

    Update contact.blade.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Contact Us')
+
+@section('content')
+    <h1>Hubungi Kami</h1>
+    <p>Anda bisa menghubungi kami melalui form di bawah ini...</p>
+@endsection
