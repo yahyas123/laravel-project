428d0028fedd4fdaf700f74bb47f814793eef4a4
MuhamadYahya <121210925+yahyas123@users.noreply.github.com>
2025-10-06 20:45:54 +0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate nam repellendus ipsa eum omnis officiis, dolorem, rerum molestias accusamus dolor sint numquam itaque officia hic dicta nemo quasi commodi nulla?</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'About Us')
+
+@section('content')
+    <h1>Tentang Kami</h1>
+    <p>Ini adalah konten untuk halaman about. Kami adalah perusahaan yang hebat...</p>
+@endsection
